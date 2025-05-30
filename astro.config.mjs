f8aed0e52d89c5efe54b143c9bd43076090fc3ca
Damianq22 <dcamiloquintero@unicesar.edu.co>
2025-05-30 16:21:54 -0500

    Eliminar la configuración de despliegue y limpiar el archivo package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 export default defineConfig({
-  output: "static", // ✅ Cambiado a "static"
-  base: "/AlquilerApp/", // ✅ Cambia esto al nombre real de tu repositorio
+  output: "static",
+  // base: "/",  // Opcional para Netlify, puedes dejarlo comentado o eliminarlo
   vite: {
     plugins: [tailwindcss()],
   },
